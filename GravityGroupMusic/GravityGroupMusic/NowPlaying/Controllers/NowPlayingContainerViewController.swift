d34202972b763125a3033b3ec6fa35abd06e9aed
Vadim Miropolskiy <vemiropolskiy@gmail.com>
2019-04-17 21:03:30 +0600

    feat(NowPlayingContainerViewControllers): embed both child view controllers

--- GravityGroupMusic/GravityGroupMusic/NowPlaying/Controllers/NowPlayingContainerViewController.swift
+++ GravityGroupMusic/GravityGroupMusic/NowPlaying/Controllers/NowPlayingContainerViewController.swift
@@ -9,14 +9,43 @@
 import UIKit
 
 class NowPlayingContainerViewController: UIViewController {
-
+    @IBOutlet private var contentView: UIView!
+    
+    private var oneSongViewController: OneSongViewController!
+    private var albumSongsListViewController: AlbumSongsListViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "NOW PLAYING"
-        //add(OneSongViewController())
-        add(AlbumSongsListViewController()f)
+        setupChildViewControllers()
     }
 
-
+    private func setupChildViewControllers() {
+        oneSongViewController = OneSongViewController()
+        add(oneSongViewController, toViewHolder: contentView)
+        
+        albumSongsListViewController = AlbumSongsListViewController()
+        add(albumSongsListViewController, toViewHolder: contentView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        let oneSongView: UIView = oneSongViewController.view
+        let songsListView: UIView = albumSongsListViewController.view
+        
+        oneSongView.translatesAutoresizingMaskIntoConstraints = false
+        songsListView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let views = ["oneSong": oneSongView, "songsList": songsListView]
+        var allConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[oneSong]-0-[songsList(200)]-0-|",
+                                                            options: [], metrics: nil, views: views)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[oneSong]-0-|",
+                                                         options: [], metrics: nil, views: views)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[songsList]-0-|",
+                                                         options: [], metrics: nil, views: views)
+        
+        NSLayoutConstraint.activate(allConstraints)
+    }
 }
 
