0e7e5db52f8c458ef134057ab830ac98b3f2984a
Vadim Miropolskiy <vemiropolskiy@gmail.com>
2019-04-22 21:37:28 +0600

    feat(PlaylistSongs): implement PlaylistSongsDataSource protocol

---
+++ GravityGroupMusic/GravityGroupMusic/NowPlaying/PlaylistSongs/DataSource/PlaylistSongsLocalDataSource.swift
@@ -0,0 +1,36 @@
+//
+//  PlaylistSongsLocalDataSource.swift
+//  GravityGroupMusic
+//
+//  Created by Vadim Miropolskiy on 4/22/19.
+//  Copyright © 2019 Gravity Group. All rights reserved.
+//
+
+import Foundation
+
+/// Implementation of PlaylistSongsDataSource protocol.
+///
+/// Returns an array of songs from a local JSON file (ignores playlistId).
+class PlaylistSongsLocalDataSource: PlaylistSongsDataSource {
+    func getPlaylistSongs(forPlaylistId playlistId: Int,
+                          completion: @escaping ([Song]?, Error?) -> Void) {
+        guard let jsonFilePath = Bundle.main.path(forResource: "playlistSongsLocalDataSource",
+                                                  ofType: "json") else { return }
+        
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: jsonFilePath),
+                                   options: []) else { return }
+        
+        do {
+            let songsDataSerialized = try JSONDecoder().decode([Song].self, from: data)
+            DispatchQueue.main.async {
+                completion(songsDataSerialized, nil)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+            
+            DispatchQueue.main.async {
+                completion(nil, error)
+            }
+        }
+    }
+}
