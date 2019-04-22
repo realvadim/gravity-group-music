73caed0e4c5ec1f700dc16f25984c6a9ab9f38b5
Vadim Miropolskiy <vemiropolskiy@gmail.com>
2019-04-22 19:35:47 +0600

    feat(Song): create model

---
+++ GravityGroupMusic/GravityGroupMusic/NowPlaying/Models/Song.swift
@@ -0,0 +1,30 @@
+//
+//  Song.swift
+//  GravityGroupMusic
+//
+//  Created by Vadim Miropolskiy on 4/22/19.
+//  Copyright © 2019 Gravity Group. All rights reserved.
+//
+
+import Foundation
+
+/// Song model.
+struct Song: Codable {
+    /// Song database id.
+    let id: Int
+    
+    /// Song name.
+    let name: String
+    
+    /// Artist/Band name.
+    let performerName: String
+    
+    /// Song cover image URL.
+    let coverImageUrl: String
+    
+    /// Playback file URL.
+    let playbackFileUrl: String
+    
+    /// Playlist database id.
+    let playlistId: Int
+}
