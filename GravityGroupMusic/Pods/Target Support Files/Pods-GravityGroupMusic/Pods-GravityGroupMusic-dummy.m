7fe42bafd7039790a38a65d55dbf001dceb4f74d
Vadim Miropolskiy <vemiropolskiy@gmail.com>
2019-04-22 20:38:35 +0600

    feat(*): install Kingfisher pod

---
+++ GravityGroupMusic/Pods/Target Support Files/Pods-GravityGroupMusic/Pods-GravityGroupMusic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GravityGroupMusic : NSObject
+@end
+@implementation PodsDummy_Pods_GravityGroupMusic
+@end
---
+++ GravityGroupMusic/Pods/Target Support Files/Pods-GravityGroupMusicTests/Pods-GravityGroupMusicTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GravityGroupMusicTests : NSObject
+@end
+@implementation PodsDummy_Pods_GravityGroupMusicTests
+@end
